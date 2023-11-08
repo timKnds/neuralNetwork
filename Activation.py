130a6853905f5eb654832fc0439c5c867db93d75
timknudsen <tim04knudsen@gmail.com>
2023-11-08 15:27:00 +0100

    added ReLu Activation Function

--- Activation.py
+++ Activation.py
@@ -7,3 +7,14 @@ def tanh(x):
 
 def tanh_prime(x):
     return 1-np.tanh(x)**2
+
+
+def relu(x):
+    return max(0, x)
+
+
+def relu_prime(x):
+    if x >= 0:
+        return 1
+    else:
+        return 0
